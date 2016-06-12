50762741b695fa3c943627242ec10b811d0e6c52
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-12 11:03:03 +0200

    Add Todo and TodoList models and associated migration.

--- todo/models.py
+++ todo/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class TodoList(models.Model):
+    # TODO: add user as a foreign key to the user table?
+    # TODO: add a 'tag' field to allow users to tag and filter lists
+    name = models.CharField(max_length=50)
+
+
+class Todo(models.Model):
+    parent_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    item_text = models.CharField(max_length=200)
+    completed = models.BooleanField()
