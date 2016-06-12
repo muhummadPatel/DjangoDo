80fe69cd2b0b826411f03f6e79095442bcb98da4
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-12 11:22:54 +0200

    Add dummy lists page to show all the lists.

--- todo/views.py
+++ todo/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'todo/index.html', {})
+
+
+def lists(request):
+    return render(request, 'todo/lists.html', {})
