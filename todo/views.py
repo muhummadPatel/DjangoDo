fd65bd82b4cb7e7047ad3640b37acc06b08629b8
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-12 12:24:44 +0200

    Add require_GET decorators to view functions.

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.decorators.http import require_GET
 
 from .models import Todo, TodoList
 
 
+@require_GET
 def index(request):
     return render(request, 'todo/index.html', {})
 
 
+@require_GET
 def lists(request):
     todo_lists = get_list_or_404(TodoList)
     context = {
@@ -15,6 +18,7 @@ def lists(request):
     return render(request, 'todo/lists.html', context)
 
 
+@require_GET
 def lists_detail(request, list_id):
     todo_list = get_object_or_404(TodoList, pk=list_id)
     todos = get_list_or_404(Todo, parent_list=list_id)
