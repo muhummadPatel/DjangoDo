26459a4e1fdd64e1a9ac9f71ea5b86def14be97c
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-12 11:51:14 +0200

    Show all available todo lists on /todo/lists page.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
+
+from .models import Todo, TodoList
 
 
 def index(request):
@@ -6,4 +8,8 @@ def index(request):
 
 
 def lists(request):
-    return render(request, 'todo/lists.html', {})
+    selected_lists = get_list_or_404(TodoList)
+    context = {
+        "list_names": [l.name for l in selected_lists]
+    }
+    return render(request, 'todo/lists.html', context)
