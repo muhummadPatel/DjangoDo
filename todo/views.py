14369b7b564ed7256c7d51a5556a06a43421b829
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-12 13:36:16 +0200

    Allow updating todo list items.

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_http_methods
 
 from .models import Todo, TodoList
 
@@ -18,14 +19,35 @@ def lists(request):
     return render(request, 'todo/lists.html', context)
 
 
-@require_GET
+@require_http_methods(['GET', 'POST'])
 def lists_detail(request, list_id):
-    todo_list = get_object_or_404(TodoList, pk=list_id)
-    todos = get_list_or_404(Todo, parent_list=list_id)
+    if request.method == 'GET':
+        todo_list = get_object_or_404(TodoList, pk=list_id)
+        todos = get_list_or_404(Todo, parent_list=list_id)
 
-    context = {
-        'todo_list': todo_list,
-        'todos': todos
-    }
+        context = {
+            'todo_list': todo_list,
+            'todos': todos
+        }
 
-    return render(request, 'todo/lists_detail.html', context)
+        return render(request, 'todo/lists_detail.html', context)
+
+    elif request.method == 'POST':
+        todo_list = get_object_or_404(TodoList, pk=list_id)
+        todos = get_list_or_404(Todo, parent_list=list_id)
+
+        completed_todos = [todo for todo in request.POST.keys()]
+        for todo in todos:
+            # TODO: Do this as a transaction. Possible race condition here? F()?
+            updated_state = str(todo.id) in completed_todos
+            if not todo.completed == updated_state:
+                todo.completed = updated_state
+                todo.save()
+
+        context = {
+            'todo_list': todo_list,
+            'todos': todos,
+            'message': "Successfully updated"
+        }
+
+        return render(request, 'todo/lists_detail.html', context)
