4829e841faf906d2a91535cc0eeacdf97a86c8ad
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-14 12:59:01 +0200

    Install django-registration and add skeleton registration form.

--- djangoDo/urls.py
+++ djangoDo/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('todo.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^todo/', include('todo.urls'))
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
