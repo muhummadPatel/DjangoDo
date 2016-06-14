a3c869c020ad17cb90bd92d0b3999d1a6a4a5739
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-14 13:10:12 +0200

    Fix root url (i.e. '/') regex.

--- djangoDo/urls.py
+++ djangoDo/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('todo.urls')),
+    url(r'^', include('todo.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
