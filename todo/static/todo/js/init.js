224c862f3a38ebf7f543ec90c7c4243fd1925e58
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-18 23:55:04 +0200

    Flesh out home.html page.

--- todo/static/todo/js/init.js
+++ todo/static/todo/js/init.js
@@ -1,7 +1,12 @@
+// collapse navbar to drawer on mobile
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// allow modals to be triggered
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
