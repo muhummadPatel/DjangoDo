de3a6c52262c37565d9f902f720ac5260322187e
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-22 01:02:10 +0200

    Add urls to enable testing error templates in DEBUG mode.

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
+from django.views.generic.base import TemplateView
 
 from . import views
+from djangoDo import settings
 
 app_name = "todo"
 urlpatterns = [
@@ -12,3 +14,11 @@ urlpatterns = [
     url(r'^lists/(?P<list_id>[\w]+)/items/(?P<item_id>[\w]+)/$', views.edit_item, name='edit_items'),
     url(r'^lists/(?P<list_id>[\w]+)/items/(?P<item_id>[\w]+)/delete/$', views.delete_item, name='delete_item'),
 ]
+
+
+# to help test 400, and 500 templates during dev
+if settings.DEBUG:
+    urlpatterns += (
+        url(r'^404/$', TemplateView.as_view(template_name='404.html'), name='test404'),
+        url(r'^500/$', TemplateView.as_view(template_name='500.html'), name='test500'),
+    )
