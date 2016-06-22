c1846e95fcf28884377c2762c0aef5d79b21b394
Muhummad Patel <muhummad.patel@gmail.com>
2016-06-22 02:00:04 +0200

    Add dj_database_url package as dependency for heroku db connectivity.

--- djangoDo/settings.py
+++ djangoDo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,16 +79,17 @@ WSGI_APPLICATION = 'djangoDo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangodo',
-        'USER': os.environ['POSTGRESQL_USER'],
-        'PASSWORD': os.environ['POSTGRESQL_PASSWORD'],
-        'HOST': os.environ['POSTGRESQL_HOST'],
-        'PORT': '',
-    }
-}
+DATABASES['default'] = dj_database_url.config()
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'djangodo',
+#         'USER': os.environ['POSTGRESQL_USER'],
+#         'PASSWORD': os.environ['POSTGRESQL_PASSWORD'],
+#         'HOST': os.environ['POSTGRESQL_HOST'],
+#         'PORT': '',
+#     }
+# }
 
 LOGIN_REDIRECT_URL = '/home'
 
